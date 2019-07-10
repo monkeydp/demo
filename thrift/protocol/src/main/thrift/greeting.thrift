a5fcc25a555b238ef7829448a6ee97c3e4482b1f
xuchen <monkeydpotato@gmail.com>
2019-07-10 17:23:40 +0800

    add gradle thrift plugin to generate code

---
+++ thrift/protocol/src/main/thrift/greeting.thrift
@@ -0,0 +1,18 @@
+namespace * com.monkeydp.demo.thrift.protocol
+
+enum TStatus {
+    MR,
+    MISS,
+    MRS,
+    MS
+}
+
+struct TName {
+    1: required string firstName;
+    2: required string secondName;
+    3: optional TStatus status;
+}
+
+service TGreetingService {
+    string greet(1: TName name);
+}
