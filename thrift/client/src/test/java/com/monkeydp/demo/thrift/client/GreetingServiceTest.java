596b199359fef2c36e889889b23b53aa874f783a
xuchen <monkeydpotato@gmail.com>
2019-07-11 14:32:42 +0800

    *

--- thrift/client/src/test/java/com/monkeydp/demo/thrift/client/GreetingServiceTest.java
+++ thrift/client/src/test/java/com/monkeydp/demo/thrift/client/GreetingServiceTest.java
@@ -1,24 +1,19 @@
 package com.monkeydp.demo.thrift.client;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 /**
+ * Start thrift server before run test
+ *
  * @author iPotato
  * @date 2019/7/11
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(
-        classes = {ThriftClientApplication.class},
-        webEnvironment = RANDOM_PORT
-)
-public class GreetingServiceTest {
+@SpringBootTest
+public class GreetingServiceTest extends BaseTest {
 
     @Autowired
     GreetingService greetingService;
