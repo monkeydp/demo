c60d75085e8d2696d9327f7fc2aec59fef38540e
xuchen <monkeydpotato@gmail.com>
2019-07-20 14:47:46 +0800

    *

---
+++ protobuf/client/src/global/Config.js
@@ -0,0 +1,12 @@
+export default class Config {
+    static SERVER = {
+        SERVER_A: {
+            host: 'localhost',
+            port: '8080',
+        },
+        SERVER_B: {
+            host: 'localhost',
+            port: '8111',
+        },
+    }
+}
--- protobuf/client/src/global/Servers.js
+++ protobuf/client/src/global/Servers.js
@@ -1,22 +1,17 @@
 import {Server} from '../tool/Server'
+import Config from './Config'
 import ProtoRoot from "../generated/js/proto"
 
 export default class Servers {
     static SERVER_A = new Server({
-        base: {
-            host: 'localhost',
-            port: '8080',
-        },
+        base: Config.SERVER.SERVER_A,
         dataDecoder: {
             protoRoot: ProtoRoot,
         }
     })
     // never used
     static SERVER_B = new Server({
-        base: {
-            host: 'localhost',
-            port: '8111',
-        },
+        base: Config.SERVER.SERVER_B,
         dataDecoder: {
             protoRoot: ProtoRoot,
         }
